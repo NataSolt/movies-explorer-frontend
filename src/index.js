1f12ee3ad557a19b06e05789dbfaddb62f151250
Natalia Sokolova <solta@inbox.ru>
2022-09-23 19:51:57 +0300

    main  по перфикт пиксель правлен

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from "react-router-dom";
 import App from './components/App/App';
+import "../src/vendor/normalize.css";
+import "../src/vendor/fonts.css";
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
