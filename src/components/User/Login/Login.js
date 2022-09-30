88aa4a8659fc84c9d037178668895b92cb9521b3
Natalia Sokolova <solta@inbox.ru>
2022-09-30 13:46:06 +0300

    register,login

--- src/components/User/Login/Login.js
+++ src/components/User/Login/Login.js
@@ -1,16 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
 import logoHeader from "../../../images/logo-header.svg";
 import "./Login.css";
 
-function Login() {
+  function Login({onLogin}) {
+    const [userData, setUserData] = useState({
+        email: '',
+        password: '',
+    });
+
+    const handleChange = (evt) => {
+        const {name, value} = evt.target;
+        setUserData({
+            ...userData, [name]: value
+        });
+    }
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        onLogin(userData);
+        setUserData({email: '', password: ''});
+    }
+
   return (
     <section className="login">
       <Link to="/">
         <img src={logoHeader} alt="Логотип" className="header__logo" />
       </Link>
       <h2 className="login__title">Рады видеть!</h2>
-      <form className="login__form">
+      <form className="login__form"  onSubmit={handleSubmit}>
         <label className="login__label">E-mail</label>
         <input
           className="login__input"
@@ -18,6 +36,8 @@ function Login() {
           type="email"
           id="email"
           required
+          value={userData.email}
+          onChange={handleChange}
         />
 
         <label className="login__label">Пароль</label>
@@ -27,6 +47,8 @@ function Login() {
           type="password"
           id="password"
           required
+          value={userData.password}
+          onChange={handleChange}
         />
 
         <button className="login__submit" type="submit">
--- src/components/User/Register/Register.js
+++ src/components/User/Register/Register.js
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logoHeader from "../../../images/logo-header.svg";
 import "./Register.css";
 
-function Register() {
+function Register({ onRegister }) {
+  const [userData, setUserData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (evt) => {
+    const { name, value } = evt.target;
+    setUserData({
+      ...userData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    onRegister(userData);
+  };
+
   return (
     <section className="register">
       <Link to="/">
         <img src={logoHeader} alt="Логотип" className="header__logo" />
       </Link>
       <h2 className="register__title">Добро пожаловать!</h2>
-      <form className="register__form">
+      <form className="register__form" onSubmit={handleSubmit}>
         <label className="register__label">Имя</label>
         <input
           className="register__input"
@@ -18,6 +37,8 @@ function Register() {
           type="text"
           id="name"
           required
+          value={userData.name}
+          onChange={handleChange}
         />
 
         <label className="register__label">E-mail</label>
@@ -27,6 +48,8 @@ function Register() {
           type="email"
           id="email"
           required
+          value={userData.email}
+          onChange={handleChange}
         />
 
         <label className="register__label">Пароль</label>
@@ -36,14 +59,16 @@ function Register() {
           type="password"
           id="password"
           required
+          value={userData.password}
+          onChange={handleChange}
         />
+
         <span className="register__span">Что-то пошло не так...</span>
 
         <button className="register__submit" type="submit">
           Зарегистрироваться
         </button>
       </form>
-
       <div className="register__container">
         <p className="register__text">Уже зарегистрированы?</p>
         <Link className="register__link" to="/sign-in">
